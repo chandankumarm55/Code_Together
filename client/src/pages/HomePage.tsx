f8965ee2ecb8e959274c1033e4629da137e2d507
Chandan Kumar <chandankumarm769@gmail.com>
2025-04-27 09:41:42 +0530

    added code

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
-import illustration from "@/assets/illustration.svg"
-import FormComponent from "@/components/forms/FormComponent"
+import FormComponent from "@/components/forms/FormComponent";
 // import Footer from "@/components/common/Footer";
 
 function HomePage() {
@@ -7,11 +6,20 @@ function HomePage() {
         <div className="flex min-h-screen flex-col items-center justify-center gap-16">
             <div className="my-12 flex h-full min-w-full flex-col items-center justify-evenly sm:flex-row sm:pt-0">
                 <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
-                    <img
-                        src={illustration}
-                        alt="Code Sync Illustration"
-                        className="mx-auto w-[250px] sm:w-[400px]"
-                    />
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold text-white sm:text-5xl">
+                            Collaborative Coding
+                        </h1>
+                        <p className="mt-2 text-lg text-white sm:text-xl">
+                            Learn and build together
+                        </p>
+                        <pre className="mt-4 font-mono text-sm text-white sm:text-base">
+{`  {}
+  // Code as a team
+  fn learn() => share()
+`}
+                        </pre>
+                    </div>
                 </div>
                 <div className="flex w-full items-center justify-center sm:w-1/2">
                     <FormComponent />
@@ -19,7 +27,7 @@ function HomePage() {
             </div>
             {/* <Footer /> */}
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
